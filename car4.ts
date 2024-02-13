d3e24309b7550216e3246b6500e6ecfff7cd83b4
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-02-13 21:28:53 +0100

    Aktualisiere pxt.json, main.blocks, main.ts, car4.ts

---
+++ car4.ts
@@ -0,0 +1,25 @@
+//% color=#007F00 icon="\uf188" block="CaR 4" weight=28
+namespace car4
+/* 240213 calliope-net.github.io/car4
+
+ */
+{
+
+
+    // ========== group="beim Start"
+
+    //% group="beim Start"
+    //% block="i2c %pADDR beim Start || i2c-Check %ck Log %pLogEnabled"
+    // pADDR.shadow="calli2bot_eADDR"
+    //% ck.shadow="toggleOnOff" ck.defl=1
+    //% pLogEnabled.shadow="toggleOnOff"
+    // blockSetVariable=Calli2bot
+    export function beimStart(pADDR: number, ck = true, pLogEnabled = false) {
+      /*   let c2 = new Calli2bot(pADDR, (ck ? true : false), pLogEnabled) // optionaler boolean Parameter kann undefined sein
+        calliBot2.c2Initialized = 1
+        calliBot2.c2IsBot2 = 1
+         */
+        
+    }
+
+}
