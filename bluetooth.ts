d27602affc785dc2c547088f466b1508c0a9a828
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-02-19 12:35:51 +0100

    Aktualisiere main.blocks, bluetooth.ts

--- bluetooth.ts
+++ bluetooth.ts
@@ -21,14 +21,13 @@ namespace car4
     }
 
     export enum eBit {
-        b0,
-        b1,
-        b2,
-        b3,
-        b4,
-        b5,
-        b6,
+        //% block="b2=0 fahren mit Joystick"
+        b2_Joystick,
+        //% block="b2>0 fahren Strecke cm"
+        b2_Encoder,
+        //% block="x80 Motor Power"
         x80_MotorPower
+
     }
 
     export function bluetooth_beimStart() {
@@ -40,20 +39,20 @@ namespace car4
 
     // ========== group="Bluetooth Verbindung" subcategory="Bluetooth"
 
-    //% group="Bluetooth Verbindung" subcategory="Bluetooth"
+    //% group="Bluetooth Verbindung" subcategory="Bluetooth" color=#E3008C
     //% block="letztes Datenpaket vor > %sekunden Sekunden" weight=4
     //% sekunden.shadow=car4_ePause
     export function lastConnected(sekunden: number) {
         return (input.runningTime() - n_lastconnectedTime) / 1000 > sekunden
     }
 
-    //% group="Bluetooth Verbindung" subcategory="Bluetooth"
+    //% group="Bluetooth Verbindung" subcategory="Bluetooth" color=#E3008C
     //% block="Bluetooth connected %connected" weight=3
     //% connected.shadow="toggleYesNo"
     export function setConnected(connected: boolean) { n_connected = connected }
 
 
-    //% group="Bluetooth Verbindung" subcategory="Bluetooth"
+    //% group="Bluetooth Verbindung" subcategory="Bluetooth" color=#E3008C
     //% block="Bluetooth connected" weight=2
     export function isConnected() { return n_connected }
 
@@ -62,14 +61,14 @@ namespace car4
 
     // ========== group="Bluetooth empfangen" subcategory="Bluetooth"
 
-    //% group="Bluetooth empfangen" subcategory="Bluetooth"
+    //% group="Bluetooth empfangen" subcategory="Bluetooth" color=#E3008C
     //% block="Datenpaket einlesen %receivedBuffer19" weight=9
     export function onReceivedBuffer(receivedBuffer19: Buffer) {
         n_receivedBuffer19 = receivedBuffer19
         n_lastconnectedTime = input.runningTime()
     }
 
-    //% group="Bluetooth empfangen" subcategory="Bluetooth"
+    //% group="Bluetooth empfangen" subcategory="Bluetooth" color=#E3008C
     //% block="Datenpaket enthält %pOffset" weight=8
     export function receivedBuffer_Contains(pOffset: eBuffer): boolean {
         if (n_receivedBuffer19 && n_receivedBuffer19.length > pOffset)
@@ -81,29 +80,21 @@ namespace car4
             return false
     }
 
-    //% group="Bluetooth empfangen" subcategory="Bluetooth"
+    //% group="Bluetooth empfangen" subcategory="Bluetooth" color=#E3008C
     //% block="Byte lesen %pOffset" weight=7
     export function receivedBuffer_getUint8(pOffset: eBuffer) {
-        if (receivedBuffer_Contains(pOffset)) {
+        if (receivedBuffer_Contains(pOffset))
             return n_receivedBuffer19.getUint8(pOffset)
-            /* let b = n_receivedBuffer19.getUint8(pOffset)
-            switch (pOffset) {
-                case eBuffer.b1_Servo: {
-                    if (between(b, 90, 135))
-                        return b
-                    else
-                        return 90
-                }
-                default: return b
-            } */
-        } else
+        else
             return 0
     }
 
-    //% group="Bluetooth empfangen" subcategory="Bluetooth"
+    //% group="Bluetooth empfangen" subcategory="Bluetooth" color=#E3008C
     //% block="Bit lesen %pBit" weight=6
     export function receivedBuffer_getBit(pBit: eBit) {
         switch (pBit) {
+            case eBit.b2_Joystick: return receivedBuffer_getUint8(eBuffer.b2_Fahrstrecke) == 0
+            case eBit.b2_Encoder: return receivedBuffer_getUint8(eBuffer.b2_Fahrstrecke) > 0
             case eBit.x80_MotorPower: return (receivedBuffer_getUint8(eBuffer.b3_Bits) & 0x80) == 0x80
 
             default: return false
@@ -111,14 +102,14 @@ namespace car4
     }
 
 
-/* 
-    // ========== group="Bluetooth senden" subcategory="Bluetooth"
-
-    //% group="Bluetooth senden" subcategory="Bluetooth"
-    //% block="sende Buffer" weight=2
-    export function sendBuffer() {
-        radio.sendBuffer(n_sendBuffer19)
-    }
- */
+    /* 
+        // ========== group="Bluetooth senden" subcategory="Bluetooth"
+    
+        //% group="Bluetooth senden" subcategory="Bluetooth"
+        //% block="sende Buffer" weight=2
+        export function sendBuffer() {
+            radio.sendBuffer(n_sendBuffer19)
+        }
+     */
 
 } // bluetooth.ts
